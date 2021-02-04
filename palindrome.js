63c184bb6aac7299c3158a8920d6a1631868aa2e
Rose Ralak <ralakrose@gmail.com>
2021-02-04 15:01:41 +0300

    more descriptive

--- palindrome.js
+++ palindrome.js
@@ -1,7 +1,7 @@
 function palindrome() {
-  let text = document.getElementById("myInput").value;
-  let reg = /[^A-Za-z0-9]/g;
-  text = text.toLowerCase().replace(reg, "");
+  let text = document.getElementById("myInput").value; // get the value input by a user
+  let reg = /[^A-Za-z0-9]/g; //regular expression to check characters
+  text = text.toLowerCase().replace(reg, ""); //convert the array of strings to lower case
   let textLength = text.length;
   for (var i = 0; i < textLength / 2; i++) {
     if (text[i] !== text[textLength - 1 - i]) {
